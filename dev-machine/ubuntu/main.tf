0c9a7869b47b5ac9a97b89a8931a47f2c10f3691
Nagarajan Ganesan <ennmail@gmail.com>
2024-02-02 21:53:23 +0100

    EC2 will custom vpc

--- dev-machine/ubuntu/main.tf
+++ dev-machine/ubuntu/main.tf
@@ -6,6 +6,44 @@ resource "aws_key_pair" "dev-machine" {
 }
 
 
+resource "aws_vpc" "dev-machine" {
+  cidr_block = "172.0.0.0/16"
+  tags = {
+    Name = "dev-machine"
+  }
+}
+
+resource "aws_subnet" "dev-machine" {
+  vpc_id     = aws_vpc.dev-machine.id
+  cidr_block = "172.0.0.0/24"
+  tags = {
+    Name = "dev-machine"
+  }
+}
+
+resource "aws_internet_gateway" "dev-machine" {
+  vpc_id = aws_vpc.dev-machine.id
+  tags = {
+    Name = "dev-machine"
+  }
+}
+
+resource "aws_route_table" "dev-machine" {
+  vpc_id = aws_vpc.dev-machine.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev-machine.id
+  }
+  tags = {
+    Name = "dev-machine"
+  }
+}
+
+resource "aws_route_table_association" "dev-machine" {
+  subnet_id      = aws_subnet.dev-machine.id
+  route_table_id = aws_route_table.dev-machine.id
+}
+
 resource "aws_security_group" "dev-machine" {
   name        = "dev-machine"
   description = "Allow SSH inbound traffic"
@@ -34,4 +72,5 @@ resource "aws_instance" "dev-machine" {
     Name = "dev-machine"
   }
   key_name = aws_key_pair.dev-machine.key_name
+
 }
